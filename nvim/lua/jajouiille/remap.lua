267193f223dbd7bac7e15f1e8462d70606bf80d7
Jamil Ettel <etteljamil267@gmail.com>
2025-10-06 14:24:47 +0200

    :sparkles: remove neozoom and change lsp signature slightly

--- nvim/lua/jajouiille/remap.lua
+++ nvim/lua/jajouiille/remap.lua
@@ -58,3 +58,7 @@ vim.api.nvim_set_keymap(
   ":lua vim.lsp.buf.rename()<CR>",
   { noremap = true, silent = true, desc = "Rename LSP" }
 )
+
+vim.keymap.set({ "n" }, "<C-k>", function()
+  require("lsp_signature").toggle_float_win()
+end, { silent = true, noremap = true, desc = "toggle signature" })
--- nvim/lua/plugins/lsp-signature.lua
+++ nvim/lua/plugins/lsp-signature.lua
@@ -11,9 +11,9 @@ return {
   -- or use config
   config = function()
     require("lsp_signature").setup({
-      -- floating_window_above_cur_line = true,
-      floating_window_off_y = -100,
-      floating_window_off_x = 2000,
+      floating_window_above_cur_line = false,
+      floating_window_off_y = 10,
+      -- floating_window_off_x = 2000,
     })
   end,
 }
