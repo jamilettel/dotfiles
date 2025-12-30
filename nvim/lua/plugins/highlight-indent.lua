0d7c1b698e8b099ca4daeeda00e264f33ef727c4
Jamil Ettel <jamil.ettel@dashdoc.com>
2025-12-30 10:43:17 +0100

    :sparkles: add new highlight indent package

--- nvim/lua/plugins/highlight-indent.lua
+++ nvim/lua/plugins/highlight-indent.lua
@@ -1,17 +1,26 @@
 return {
-  "shellRaining/hlchunk.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("hlchunk").setup({
+	"nvimdev/indentmini.nvim",
+	config = function()
+		require("indentmini").setup()
 
-      chunk = {
-        enable = true,
-        use_treesitter = true,
-      },
-      indent = {
-        use_treesitter = true,
-        enable = true,
-      },
-    })
-  end,
+		vim.cmd.highlight("IndentLine guifg=#444444")
+		vim.cmd.highlight("IndentLineCurrent guifg=#9C5E59")
+	end,
 }
+-- return {
+--   "shellRaining/hlchunk.nvim",
+--   event = { "BufReadPre", "BufNewFile" },
+--   config = function()
+--     require("hlchunk").setup({
+--
+--       chunk = {
+--         enable = true,
+--         use_treesitter = true,
+--       },
+--       indent = {
+--         use_treesitter = true,
+--         enable = true,
+--       },
+--     })
+--   end,
+-- }
